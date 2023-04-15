c337f6ea196279ad8add7dabe5392fefc5b00300
Alyani Mamad <alyanimamadbhai@gmail.com>
2023-04-15 14:25:56 +0530

    Files Updated

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from "react"
+import Resturant from "./component/Basics/Resturant"
+import UseState from "./component/Basics/Navbar"
+
+const App = () => {
+  return <Resturant />
+}
+
+export default App;
